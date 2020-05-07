1cb3191038c50e49c92e9038946517d906a4dfc4
AzraelVT <37254945+AzraelVT@users.noreply.github.com>
2020-05-07 13:21:55 -0500

    Add files via upload

---
+++ IdeoGo.API/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using IdeoGo.API.Domain.Models;
+using IdeoGo.API.Resources;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoGo.API.Mapping
+{
+    public class ResourceToModelProfile : AutoMapper.Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SaveTagResource, Goal>();
+        }
+    }
+}
+
