fe6d3b94da888b8c6e7bb68c307cdc5e7692c756
nivra <nivraleak@outlook.com>
2020-06-09 02:02:33 -0500

    Agregando relaciones

--- IdeoGo.API/Domain/Models/Tag.cs
+++ IdeoGo.API/Domain/Models/Tag.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace IdeoGo.API.Domain.Models
 {
     public class Tag
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
