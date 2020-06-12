6c3180f26a1d0ca450f70f6fe0402ec1a7d4e985
William <wmavalle@gmail.com>
2020-06-11 19:53:57 -0500

    TB04 IS COMING

---
+++ IdeoGo.API/Domain/Repositories/IMembershipRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoGo.API.Domain.Repositories
+{
+    interface IMembershipRepository
+    {
+        Task<DateTime> AddAsync();
+        Task<int> GetIdMembershipAsync(DateTime now);
+    }
+}
