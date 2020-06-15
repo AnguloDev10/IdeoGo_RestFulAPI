6ae47ba92abbd3b47f58a096b3d53b74ab68f2db
William <wmavalle@gmail.com>
2020-06-15 01:42:58 -0500

    tb04/v2

---
+++ IdeoGo.API/Domain/Repositories/IAppoitmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using IdeoGo.API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoGo.API.Domain.Repositories
+{
+     public interface IAppoitmentRepository
+    {
+        Task<IEnumerable<Appoitment>> ListAsync();
+        Task AddAsync(Appoitment appoitment);
+        void Update(Appoitment appoitment);
+        void Remove(Appoitment appoitment);
+        Task<Appoitment> FindByIDAsync(int id);
+    }
+}
