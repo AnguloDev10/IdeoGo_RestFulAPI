3f81a95a42f813cb523689925fea002fed96f0b0
William <wmavalle@gmail.com>
2020-06-15 01:54:28 -0500

    tb04/v3

---
+++ IdeoGo.API/Controllers/AppoitmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoGo.API.Controllers
+{
+
+
+    [Produces("application/json")]
+    [ApiController]
+    public class AppoitmentController :Controller
+    {
+    }
+}
---
+++ IdeoGo.API/Services/AppoitmentService.cs
@@ -0,0 +1,33 @@
+﻿using IdeoGo.API.Domain.Models;
+using IdeoGo.API.Domain.Services;
+using IdeoGo.API.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoGo.API.Services
+{
+    public class AppoitmentService : IAppoitmentService
+    {
+        public async Task<AppoitmentResponse> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Appoitment>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AppoitmentResponse> SaveAsync(Appoitment appoitment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AppoitmentResponse> UpdateAsync(int id, Appoitment appoitment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
