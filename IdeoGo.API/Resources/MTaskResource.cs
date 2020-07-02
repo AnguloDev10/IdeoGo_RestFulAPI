15ff2d8eac24b2cc400f188b57db941b998005c9
Sergio Alva <49848146+Setsuke@users.noreply.github.com>
2020-07-02 15:40:52 -0500

    MTaskResource Fix

--- IdeoGo.API/Resources/MTaskResource.cs
+++ IdeoGo.API/Resources/MTaskResource.cs
@@ -10,5 +10,7 @@ namespace IdeoGo.API.Resources
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int ProjectScheduleId { get; set; }
+        public int ProjectId { get; set; }
     }
 }
